da13e6def607f27e1f194d4b5ea2c306a07aff23
Othmane Imejad <oimejad@e1r1p8.1337.ma>
2022-06-04 00:29:32 +0100

    m

--- Vector/Vector.hpp
+++ Vector/Vector.hpp
@@ -11,35 +11,39 @@ namespace ft
     {
         private:
             T *arr;
-            int size;
-            int index;
+            int mysize;
+            int mycapacity;
             unsigned int maxsize;
         public:
             typedef T* iterator;
+            typedef std::reverse_iterator<iterator> reverse_iterator;
 
             Vector()
             {
                 arr = new T[1];
-                size = 1;
-                index = 0;
+                arr[0] = 0;
+                mysize = 0;
+                mycapacity = 2;
                 maxsize = 1073741823;
             }
 
             Vector(T n)
             {
                 arr = new T[n];
-                size = 1;
-                index = 0;
+                for (int i = 0; i < n; i++)
+                    arr[i] = 0;
+                mysize = n;
+                mycapacity = n;
                 maxsize = 1073741823;
             }
 
             Vector(const Vector<T> &copy)
             {
-                arr = new T[copy.getSize()];
-                for (int i = 0; i < copy.getSize() ; i++)
+                arr = new T[copy.size()];
+                for (int i = 0; i < copy.size() ; i++)
                     arr[i] = copy.getArray()[i];
-                size = copy.getSize();
-                index = getIndex();
+                mysize = copy.size();
+                mycapacity = copy.capacity();
             }
 
             ~Vector()
@@ -50,23 +54,21 @@ namespace ft
             Vector & operator=(const Vector &assign)
             {
                 delete arr;
-                arr = new T[assign.getSize()];
-                for (int i = 0; i < assign.getSize() ; i++)
+                arr = new T[assign.size()];
+                for (int i = 0; i < assign.size() ; i++)
                     arr[i] = assign.getArray()[i];
+                mysize = assign.size();
+                mycapacity = assign.capacity();
                 return *this;
             }
             
-            int getSize() const
+            int size() const
             {
-                return size;
+                return mysize;
             }
             T* getArray() const
             {
                 return arr;
-            }
-            int getIndex() const
-            {
-                return index;
             }
             unsigned int max_size()
             {
@@ -80,10 +82,90 @@ namespace ft
 
             iterator end()
             {
-                return arr+size;
+                return arr+mysize;
             }
 
-            
+            reverse_iterator rbegin()
+            {
+                return reverse_iterator(end());
+            }
+
+            reverse_iterator rend()
+            {
+                return reverse_iterator(begin());
+            }
+
+            void resize(int s, int v = 0)
+            {
+                if (s < mysize)
+                    mysize = s;
+                if (mysize < s)
+                {
+                    T* tmp = new T[s];
+                    for (int i = 0; i < mysize; i++)
+                        tmp[i] = arr[i];
+                    for (int i = mysize; i < s; i++)
+                        tmp[i] = v;
+                    delete arr;
+                    arr = tmp;
+                    mysize = s;
+                }
+            }
+
+            int capacity() const
+            {
+                return mycapacity;
+            }
+
+            bool empty()
+            {
+                if (mysize == 0)
+                    return true;
+                return false;
+            }
+
+            void reserve(int n)
+            {
+                if (n > mycapacity)
+                {
+                    T* tmp = new T[n];
+                    for (int i = 0; i < mysize; i++)
+                        tmp[i] = arr[i];
+                    delete arr;
+                    arr = tmp;
+                    mycapacity = n;
+                }
+            }
+
+            T &operator[](int i)
+            {
+                return arr[i];
+            }
+
+            struct indexOutOfBound : public std::exception
+            {
+                const char * what () const throw ()
+                {
+                    return "Index Out Of Bound !";
+                }
+            };
+
+            T at(int i)
+            {
+                if (i >= mysize)
+                    throw indexOutOfBound();
+                return arr[i];
+            }
+
+            T front()
+            {
+                return arr[0];
+            } 
+
+            T back()
+            {
+                return arr[mysize-1];
+            }
     };
 }
 
--- Vector/main.cpp
+++ Vector/main.cpp
@@ -1,5 +1,6 @@
 #include "Vector.hpp"
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -7,13 +8,45 @@ int main()
 {
     ft::Vector<int> f(5);
     f.getArray()[0] = 1337;
-    // ft::Vector<int> c(f);
-    // cout << c.getArray()[0] << "\n";
-    // ft::Vector<int> d = c;
-    // cout << d.getArray()[0] << "\n";
+    f.getArray()[1] = 1444;
+    ft::Vector<int> c(f);
+    cout << c.getArray()[0] << "\n";
+    ft::Vector<int> d = c;
+    cout << d.getArray()[0] << "\n";
 
-    for (ft::Vector<int>::iterator i = f.begin(); i < f.end(); i++)
+    for (ft::Vector<int>::reverse_iterator i = f.rbegin(); i < f.rend(); i++)
+    {
         cout << *i << endl;
-    
+    }
+
+    vector<int> vec(10);
+    vec.resize(20,11);
+    for (vector<int>::iterator i = vec.begin(); i < vec.end(); i++)
+    {
+        cout << *i << ' ';
+    }
+    cout << endl;
+    ft::Vector<int> ve(10);
+    ve.resize(20,11);
+    for (ft::Vector<int>::iterator i = ve.begin(); i < ve.end(); i++)
+    {
+        cout << *i << ' ';
+    }
+    cout << endl;
+
+    ft::Vector<int> em;
+    cout << em.empty() << endl;
+    cout << ve.empty() << endl;
+    ve.reserve(70);
+
+    ft::Vector<int> ffffr(5);
+    cout << ffffr[4] << endl;
+
+    ft::Vector<int> fffr(5);
+    fffr[4] = 555;
+    cout << fffr.at(4) << endl;
+
+    cout << fffr.front() - fffr.back() << endl;
+
     return 0;
 }
